fd70f4fea83e049d1d49319b03ccb23807ca822c
Seth <s.scottvancouverwa@gmail.com>
2022-02-04 22:29:35 -0800

    print server activity to terminal

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import discord
 import os
+import time
 from dotenv import load_dotenv
 load_dotenv()
-
 
 TOKEN = os.getenv("TOKEN")
 
@@ -10,17 +10,22 @@ client = discord.Client()
 print("Starting Discord bot..")
 print(f"VERSION: {discord.__version__}")
 
+
 @client.event
 async def on_ready():
     print("{0.user} is online!".format(client))
 
+
 @client.event
 async def on_message(message):
+    # prints username, time and message to terminal for debug
+    print(f"{message.author} {time.strftime('%H:%M:%S')}: {message.content}")
+
     if message.author == client.user:
         return
 
-    elif message.content.startswith("!hello") or message.content.startswith("/hello"):
-        await message.channel.send("/tts hello from a robot!")
+    elif message.content.startswith("/hello"):
+        await message.channel.send("hello!")
 
 
 client.run(TOKEN)
